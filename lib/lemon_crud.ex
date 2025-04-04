9fe71cea38255121afaed5464f797862e595ac67
Michał Buszkiewicz <michal@curiosum.com>
2025-04-04 14:10:05 +0200

    Initial commit

---
+++ lib/lemon_crud.ex
@@ -0,0 +1,18 @@
+defmodule LemonCrud do
+  @moduledoc """
+  Documentation for `LemonCrud`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LemonCrud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lemon_crud_test.exs
@@ -0,0 +1,8 @@
+defmodule LemonCrudTest do
+  use ExUnit.Case
+  doctest LemonCrud
+
+  test "greets the world" do
+    assert LemonCrud.hello() == :world
+  end
+end
