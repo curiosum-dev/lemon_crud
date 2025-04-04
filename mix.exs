feb9b1695f113078cc46b7db8804fceeb1bf6a86
Michał Buszkiewicz <michal@curiosum.com>
2025-04-04 14:39:30 +0200

    chore: update project setup and deps

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule LemonCrud.MixProject do
   def project do
     [
       app: :lemon_crud,
+      description:
+        "Create uniform yet flexible CRUD functions for your Phoenix contexts to reduce generated boilerplate.",
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -21,8 +25,25 @@ defmodule LemonCrud.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:versioce, "~> 2.0.0", only: :dev, runtime: false},
+      {:ecto, "~> 3.0"},
+      {:ecto_sql, "~> 3.0", optional: true},
+      {:postgrex, ">= 0.0.0", optional: true, only: [:dev, :test]}
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Curiosum"],
+      links: %{"GitHub" => "https://github.com/curiosum-dev/lemon_crud"},
+      files: ~w(lib mix.exs README.md LICENSE)
     ]
   end
 end
